b6d269ec19b33a261f3a6c41fa1eb218bcfefb21
Ghanbode123 <133061973+Ghanbode123@users.noreply.github.com>
2023-10-12 11:52:15 -0400

    finished step3

---
+++ GhanbodeWebbApp/Controllers/SampleController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace GhanbodeWebbApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+         public IActionResult Todolist(int id, string title, string description)
+        {
+            
+            return View();
+        }
+    }
+    
+}
