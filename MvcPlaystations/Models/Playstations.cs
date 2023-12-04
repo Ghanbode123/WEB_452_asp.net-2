c2267aed8e942691e61fd064a35f35853d537ad1
Ghanbode123 <133061973+Ghanbode123@users.noreply.github.com>
2023-12-04 14:31:44 -0500

    step 1 done

--- MvcPlaystations/Models/Playstations.cs
+++ MvcPlaystations/Models/Playstations.cs
@@ -4,11 +4,13 @@ namespace MvcPlaystations.Models;
     public class Playstations
 {
     public int Id { get; set; }
-    public string ModelName { get; set; }
-    public string Manufacturer { get; set; }
+    public string? ModelName { get; set; }
+    public string? Manufacturer { get; set; }
     public int StorageCapacityGB { get; set; }
     public decimal Price { get; set; }
     public DateTime ReleaseDate { get; set; }
+    public decimal Version { get; set; }
+     public string? Colour { get; set; }
   
 }
 
