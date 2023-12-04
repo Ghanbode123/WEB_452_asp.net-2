7c38fcb9fd3fbfc33595b7b6fedf8879ca0c0099
Ghanbode123 <133061973+Ghanbode123@users.noreply.github.com>
2023-12-04 14:27:39 -0500

    commit

--- MvcPlaystations/Models/Playstations.cs
+++ MvcPlaystations/Models/Playstations.cs
@@ -1,14 +1,16 @@
 using System.ComponentModel.DataAnnotations;
 namespace MvcPlaystations.Models;
 
-    public class Playstations
+public class Playstations
 {
     public int Id { get; set; }
-    public string ModelName { get; set; }
-    public string Manufacturer { get; set; }
+    public string? ModelName { get; set; }
+    public string? Manufacturer { get; set; }
     public int StorageCapacityGB { get; set; }
     public decimal Price { get; set; }
     public DateTime ReleaseDate { get; set; }
-  
+    public decimal Version { get; set; }
+    public string? Colour { get; set; }
+
 }
 
